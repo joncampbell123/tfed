9ded46cc21d3ff82bb435d330ab47c3eed9cc8fa
Jonathan Campbell <jonathan@castus.tv>
2018-07-15 22:41:48 -0700

    more

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,40 @@
 
 #include <stdio.h>
 
+#include <map>
+#include <vector>
+
+class TFSequence {
+public:
+                                                TFSequence();
+    virtual                                     ~TFSequence();
+protected:
+    std::string                                 name;
+};
+
+using TFSequenceListBaseType = std::map<std::string, TFSequence*>;
+
+class TFSequenceList : protected TFSequenceListBaseType {
+public:
+                                                TFSequenceList();
+    virtual                                     ~TFSequenceList();
+public:
+    TFSequence&                                 new_sequence(const std::string &name);
+    TFSequence&                                 get_sequence(const std::string &name);
+    bool                                        delete_sequence(const std::string &name);
+    bool                                        sequence_exists(const std::string &name) const;
+    bool                                        rename_sequence(const std::string &oldname,const std::string &newname);
+protected: /* the base class is protected */
+};
+
+class TFProject {
+public:
+                                                TFProject();
+    virtual                                     ~TFProject();
+public:
+    TFSequenceList                              sequences;
+};
+
 int main() {
     return 0;
 }
