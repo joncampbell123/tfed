35332cd260efde4658094fe6292e79273c28987d
Jonathan Campbell <jonathan@castus.tv>
2018-07-16 00:39:47 -0700

    more

--- test1.cpp
+++ test1.cpp
@@ -39,6 +39,27 @@ int main() {
             std::cout << "Sequence: " << i.first << std::endl;
     }
 
+    {
+        TFSequence &x = proj.sequences["seq3"];
+        TFTrackGrouping &l = x.groupings.create("x1");
+        l.push_back("video1");
+        l.push_back("video2");
+    }
+    {
+        for (auto &i : proj.sequences["seq3"].groupings)
+            std::cout << "grouping: " << i.first << std::endl;
+    }
+    {
+        TFSequence &x = proj.sequences["seq3"];
+        TFTrackGrouping &l = x.groupings["x1"];
+        l.clear();
+    }
+    proj.sequences["seq3"].groupings.erase("x1");
+    {
+        for (auto &i : proj.sequences["seq3"].groupings)
+            std::cout << "grouping: " << i.first << std::endl;
+    }
+ 
     return 0;
 }
 
